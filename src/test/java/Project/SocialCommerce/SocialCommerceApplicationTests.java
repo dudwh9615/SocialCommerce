d163da91a04c1ddbfb9d4b56922eaa268d817386
dudwh9615 <dudwh9615@naver.com>
2024-01-24 23:50:48 +0900

    스프링 프로젝트 생성

---
+++ src/test/java/Project/SocialCommerce/SocialCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.SocialCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
