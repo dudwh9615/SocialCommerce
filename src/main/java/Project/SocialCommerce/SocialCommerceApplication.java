a912b5cb3d4a6a4cee204e3c6b9f422e2756a160
dudwh9615 <dudwh9615@naver.com>
2024-01-28 02:26:20 +0900

    사용하지 않는 import 삭제

--- src/main/java/Project/SocialCommerce/SocialCommerceApplication.java
+++ src/main/java/Project/SocialCommerce/SocialCommerceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan(basePackages = "Project.SocialCommerce.model")
