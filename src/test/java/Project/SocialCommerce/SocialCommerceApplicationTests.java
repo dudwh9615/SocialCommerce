3f62446d852049230ddfa38dde7e23d27ad230fc
dudwh9615 <dudwh9615@naver.com>
2024-01-28 02:24:57 +0900

    -

--- src/test/java/Project/SocialCommerce/SocialCommerceApplicationTests.java
+++ src/test/java/Project/SocialCommerce/SocialCommerceApplicationTests.java
@@ -3,7 +3,7 @@ package Project.SocialCommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SocialCommerceApplication.class)
 class SocialCommerceApplicationTests {
 
 	@Test
