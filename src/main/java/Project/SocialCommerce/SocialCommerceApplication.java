4544f95774ca11d4bac49195bb7289a556ae8162
dudwh9615 <dudwh9615@naver.com>
2024-01-28 02:24:39 +0900

    시큐리티 기본 화면 표시 중지 엔티티 위치 설정

--- src/main/java/Project/SocialCommerce/SocialCommerceApplication.java
+++ src/main/java/Project/SocialCommerce/SocialCommerceApplication.java
@@ -2,8 +2,12 @@ package Project.SocialCommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EntityScan(basePackages = "Project.SocialCommerce.model")
 public class SocialCommerceApplication {
 
 	public static void main(String[] args) {
