4f28fcad81224ba77e2cc2a84a07b540e2201786
dudwh9615 <dudwh9615@naver.com>
2024-01-28 16:38:15 +0900

    회원 가입 시 email 유효성 및 빈 값에 대한 거절 처리

--- src/main/java/Project/SocialCommerce/dto/RegisterRequestDto.java
+++ src/main/java/Project/SocialCommerce/dto/RegisterRequestDto.java
@@ -1,12 +1,10 @@
 package Project.SocialCommerce.dto;
 
-import Project.SocialCommerce.model.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
