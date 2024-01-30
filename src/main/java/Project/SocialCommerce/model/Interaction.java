70d8421f2d6c2c62f8e64194f7acefdf9ad49dba
dudwh9615 <dudwh9615@naver.com>
2024-01-30 15:08:48 +0900

    상호작용 관련 구현

---
+++ src/main/java/Project/SocialCommerce/model/Interaction.java
@@ -0,0 +1,31 @@
+package Project.SocialCommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "interactions")
+@Entity
+@NoArgsConstructor
+public class Interaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+
+}
