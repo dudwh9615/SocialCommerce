63328099bbe8859a5b7d2a25b54b5622962b3d20
dudwh9615 <dudwh9615@naver.com>
2024-02-01 10:42:48 +0900

    댓글 생성 기능 구현

--- src/main/java/Project/SocialCommerce/controller/CommentController.java
+++ src/main/java/Project/SocialCommerce/controller/CommentController.java
@@ -1,4 +1,35 @@
 package Project.SocialCommerce.controller;
 
+import Project.SocialCommerce.dto.CommentingRequestDto;
+import Project.SocialCommerce.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<String> commenting(@RequestBody CommentingRequestDto requestDto, Principal principal) {
+        commentService.addComment(requestDto, principal.getName());
+        return ResponseEntity.ok("게시 완료");
+    }
+
+//    @GetMapping("/{postId}")
+//    public ResponseEntity<PostResponseDto> getPost(@PathVariable Long postId) {
+//        return ResponseEntity.ok(postService.getPost(postId));
+//    }
+
+    @PutMapping
+    public ResponseEntity<String> editPost(@RequestBody EditCommentRequestDto requestDto, Principal principal) {
+        commentService.editComment(requestDto, principal.getName());
+        return ResponseEntity.ok("게시글 수정 완료");
+    }
 }
